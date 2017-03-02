38ad23d044f860352930361daefa93a86f8fce24
John Plastow <j2trumpet@gmail.com>
2017-03-02 13:40:03 -0800

    Starting post page

---
+++ mpdx_marketing/post.php
@@ -0,0 +1,49 @@
+<?php include 'functions.php'; ?>
+<?php the_header('Post Page', 'post', 'mpdx-pages'); ?>
+
+  <section class="spotlight spotlight--text-center spotlight--overlay prod-cat1" style="background-image: url(assets/img/MPDX_product_page_hero.jpg);">
+    <div class="prod-img-overlay prod-cat1">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <img src="assets/img/solid_featured_icon.svg" alt="" />
+            <h2>Sync Your Prayerletter Services</h2>
+            <p class="date">November 2, 2016</p>
+            <p class="author">Emily van den Heever</p>
+            <div class="thumb" style="background-image: url(http://s3images.coroflot.com/user_files/individual_files/avatars/large_avatar_509597_dpsovk0vwzv8el3rrooe8qrw2.jpg);"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="post-content post-content--padded-tb-2g">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vehicula libero in dolor condimentum eleifend eget ac dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean finibus arcu mi, non tempor turpis pellentesque at. Donec ultrices feugiat purus, et porta dolor gravida ac. Aliquam erat volutpat. Nunc in sapien ac mauris dapibus faucibus. Suspendisse id facilisis eros, a faucibus justo. Nam faucibus ultrices nunc. Maecenas vehicula sodales ligula et consectetur. Fusce vitae porttitor justo, at faucibus dui. Curabitur vel sollicitudin nulla. Phasellus fringilla risus eget urna aliquet congue.</p>
+          <p>Vivamus commodo ac justo non luctus. Duis pellentesque est et massa ultricies mollis. Integer sit amet risus nunc. Sed id consectetur risus. Ut ullamcorper magna non massa porttitor auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tellus sollicitudin, fermentum diam nec, aliquet nisi. Fusce accumsan leo ante, eget cursus lectus aliquam et.</p>
+          <p>Sed luctus lacinia nisl sit amet lobortis. Quisque ultricies sapien non ultrices egestas. Vivamus tortor sem, iaculis quis venenatis id, lacinia ac arcu. Morbi vestibulum vehicula ultricies. In hac habitasse platea dictumst. Aliquam in gravida dui. Praesent elementum finibus auctor. Aenean quis quam volutpat, fermentum nunc eget, porttitor odio.</p>
+          <p>Morbi eleifend, nulla eget laoreet tristique, magna lectus congue elit, quis scelerisque odio nibh eget diam. Aliquam imperdiet non mauris in molestie. Praesent et nisi mauris. Nullam aliquam, metus in molestie blandit, leo ante efficitur sem, a porttitor tortor eros vel lorem. Maecenas finibus in tellus sed feugiat. Proin accumsan ante sit amet porttitor aliquet. Vestibulum at metus congue nulla laoreet rhoncus quis sit amet ex. Suspendisse nec odio facilisis, egestas tellus sit amet, ullamcorper massa. Ut vel dui congue, mollis purus a, condimentum arcu. Proin aliquet mauris sit amet volutpat mattis. Curabitur aliquam sapien quis malesuada imperdiet. Proin rutrum efficitur dui, eu posuere diam bibendum at. Suspendisse ac ex quam. Nullam commodo justo magna, sit amet blandit neque interdum nec. Nunc gravida tempor magna, eu aliquam augue venenatis id. Nam at efficitur quam.</p>
+          <p>Sed est enim, vestibulum quis tortor ultricies, sollicitudin aliquam orci. Donec eget magna tincidunt, vulputate ligula sed, egestas nunc. Proin sit amet nisi sagittis, mattis risus vel, efficitur urna. Praesent sit amet neque quis leo accumsan lacinia. Aenean in scelerisque ante. Phasellus tristique nec erat non vestibulum. Vivamus non magna purus. Maecenas nec nisi urna. In scelerisque eros nec mi malesuada, vitae accumsan urna lacinia. In sollicitudin congue ante pellentesque convallis. Ut malesuada odio non felis aliquet, quis facilisis risus interdum. Aliquam ultrices molestie massa eu maximus. Duis lobortis vehicula condimentum. Ut leo turpis, semper ac feugiat ac, fermentum vitae velit.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="post-author">
+    <div class="container">
+      <div class="row row--padded-tb-2g">
+        <div class="col-md-2 col-md-offset-1">
+          Photo
+        </div>
+        <div class="col-md-8">
+          <h3>About Emily van den Heever</h3>
+          <p>Emily is a US Designer for CRU, etc. Sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribu.</p>
+          <p class="inline-link"><a href="#">View all posts by Emily</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php the_footer(); ?>
